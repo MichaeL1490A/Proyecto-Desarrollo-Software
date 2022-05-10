d658be259c8a2d068da531bd5ffc7eb5c1a63878
Hiteek <ruben.ricapa.c@uni.pe>
2022-05-10 09:45:10 -0500

    clase tabla renombrada

--- src/sprint1_0/product/board.py
+++
@@ -1,6 +0,0 @@
-import pygame
-class Table(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load("table.jpeg").convert()
-        self.rect = self.image.get_rect()
---
+++ src/sprint1_0/product/table.py
@@ -0,0 +1,6 @@
+import pygame
+class Table():
+    def __init__(self):
+        self.board = [] # Fichas en juego
+        self.brown_left = self.white_left = 9 # Numero de fichas que quedan
+    
