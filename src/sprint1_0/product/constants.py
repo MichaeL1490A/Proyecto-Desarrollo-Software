c7ed61fa50a02eb87ac45c3cd11996be55b31c6f
Hiteek <ruben.ricapa.c@uni.pe>
2022-05-10 16:18:46 -0500

    Añadido tabla con las posiciones y tamaño de los espacios del tablero

--- src/sprint1_0/product/constants.py
+++ src/sprint1_0/product/constants.py
@@ -3,8 +3,18 @@ import pygame
 SCREEN_WIDTH = 1020
 SCREEN_HEIGHT = 1020
 FIL,COL=7,7
+SIZE = SCREEN_WIDTH//COL
 screen = pygame.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])
 #Colores
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 BROWN = (115,0,0)
+tablita =[
+            [True,False,False,True,False,False,True],
+            [False,True,False,True,False,True,False],
+            [False,False,True,True,True,False,False],
+            [True,True,True,False,True,True,True],
+            [False,False,True,True,True,False,False],
+            [False,True,False,True,False,True,False],
+            [True,False,False,True,False,False,True],
+        ]
