a739c71f33f0450c084b884c80bcecd7a20ec04f
Hiteek <ruben.ricapa.c@uni.pe>
2022-05-10 09:15:26 -0500

    constantes añadidas

--- src/sprint1_0/product/constants.py
+++ src/sprint1_0/product/constants.py
@@ -1,5 +1,10 @@
-#PRUEBA
+import pygame
+#Dimensiones del programa
 SCREEN_WIDTH = 1020
 SCREEN_HEIGHT = 1020
+FIL,COL=7,7
+screen = pygame.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])
+#Colores
 WHITE = (255,255,255)
 BLACK = (0,0,0)
+BROWN = (115,0,0)
