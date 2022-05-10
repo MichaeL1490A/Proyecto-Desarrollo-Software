86fff454773cb838bd6c0246558e98e49fa06c60
Hiteek <ruben.ricapa.c@uni.pe>
2022-05-10 09:22:41 -0500

    Clase ficha modificada

--- src/sprint1_0/product/ficha.py
+++ src/sprint1_0/product/ficha.py
@@ -1,13 +1,10 @@
 import pygame
 from constants import *
 #CLASE FICHA
-class Ficha(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load("player.png").convert()
-        self.image.set_colorkey(BLACK)
-        self.rect = self.image.get_rect()
-    def move(self):
-        mouse_pos = pygame.mouse.get_pos()
-        self.rect.x = mouse_pos[0]
-        self.rect.y = mouse_pos[1]
+class Ficha():
+    def __init__(self,fil,col,color):
+        self.fil = fil
+        self.col = col
+        self.color = color 
+        self.x = 0
+        self.y = 0
