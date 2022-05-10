96698f6258ebf0a29e89d02263032f69baae1e1a
Hiteek <ruben.ricapa.c@uni.pe>
2022-05-10 09:42:29 -0500

    main añadido

---
+++ src/sprint1_0/product/main.py
@@ -0,0 +1,19 @@
+import pygame
+from game import Game
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])
+    done = False 
+    clock = pygame.time.Clock()
+    game = Game(screen)
+    while not done:
+        done = game.process_events()
+        game.display_frame(screen)
+        clock.tick(60)
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
+    
