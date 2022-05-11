5b486897380e110ad67b755f387442221fa9fe4a
JDomCamZ <84546063+JDomCamZ@users.noreply.github.com>
2022-05-11 13:23:34 -0500

    Implementando test de calcular posicion.

--- src/sprint1_0/test/test_ficha.py
+++ src/sprint1_0/test/test_ficha.py
@@ -1,8 +1,10 @@
 import unittest
-from sprint1_0.product.constants import GREY, WHITE
+from sprint1_0.product.constants import *
 from sprint1_0.product.ficha import *
 
 class TestFicha(unittest.TestCase):
-    a=Ficha(1,2,GREY)
     def test_calcular_posicion(self):
-        self.assertAlmostEqual()
+        a = Ficha(3,2,GREY)
+        x = SIZE * 2 + SIZE//2
+        y = SIZE * 3 + SIZE//2
+        self.assertAlmostEqual((x,y),(a.x,a.y))
