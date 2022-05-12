d6b1e0dad07445949be71f5205d82f6fe1e8ca3a
MichaeL1490A <gcarpiou@uni.pe>
2022-05-11 23:10:45 -0500

    Se diseña la tabla, en archivo table.py

--- src/sprint1_0/product/ficha.py
+++ src/sprint1_0/product/ficha.py
@@ -1,23 +1,29 @@
 import pygame
 from constants import SIZE
-#CLASE FICHA
+# CLASE FICHA
+
+
 class Ficha():
-    def __init__(self,fil,col,color):
+    def __init__(self, fil, col, color):
         self.fil = fil
         self.col = col
-        self.color = color 
+        self.color = color
         self.x = 0
         self.y = 0
         self.calcular_posicion()
-    #Calcula la posicion en la pantalla
+    # Calcula la posicion en la pantalla
+
     def calcular_posicion(self):
-        self.x = SIZE * self.col + SIZE//2
-        self.y = SIZE * self.fil + SIZE//2
-    def draw(self,screen):
-        pygame.draw.circle(screen,self.color,(self.x,self.y),20)
-    def move(self,fil,col):
+        self.x = SIZE*self.col + SIZE//2
+        self.y = SIZE*self.fil + SIZE//2
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, (self.x, self.y), 20)
+
+    def move(self, fil, col):
         self.fil = fil
-        self.col = col 
+        self.col = col
         self.calcular_posicion()
+
     def __repr__(self):
         return str(self.color)
--- src/sprint1_0/product/game.py
+++ src/sprint1_0/product/game.py
@@ -1,37 +1,44 @@
-import pygame, sys
+import pygame
+import sys
 from table import Table
-from constants import BROWN,WHITE,GREY,screen,SIZE,tablita
+from constants import BROWN, WHITE, GREY, screen, SIZE, tablita
 from ficha import Ficha
 
 sys.path.append("..\\..\\Proyecto-Desarrollo-Software")
 
+
 def get_row_col_from_mouse(pos):
     x, y = pos
-    row = (y - SIZE //2)//SIZE
-    col = (x - SIZE//2)//SIZE
-    return row,col
+    row = (y-SIZE // 2) // SIZE
+    col = (x-SIZE // 2) // SIZE
+    return row, col
 
 
-#CLASE JUEGO
+# CLASE JUEGO
 class Game():
     def __init__(self, screen):
         self._init()
         self.screen = screen
+
     def _init(self):
         self.table = Table()
         self.contador = 0
-        #self.ficha = Ficha(1,1,BROWN) #PRUEBA
+        # self.ficha = Ficha(1,1,BROWN) #PRUEBA
         self.turn = GREY
+
     def update(self):
-        self.table.dibujar(screen)
-        #self.ficha.draw(screen) #PRUEBA
+        self.table.draw_screen(screen)
+        # self.ficha.draw(screen) #PRUEBA
         pygame.display.update()
+
     def cambiar_turno(self):
         if self.turn == GREY:
-            self.turn = BROWN
+            self.turn = WHITE
         else:
             self.turn = GREY
-    def process_events(self):
+
+    def process_events(self, screen):
+        screen.fill(BROWN)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return True
@@ -44,14 +51,13 @@ class Game():
                     self.ficha.move(fil,col)
             '''
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pos = pygame.mouse.get_pos()
-                fil,col = get_row_col_from_mouse(pos)
+                mouse = pygame.mouse.get_pos()
+                fil, col = get_row_col_from_mouse(mouse)
                 if fil >= 0 and col >= 0:
                     if tablita[fil][col] == True and self.table.board[fil][col] == 0 and self.contador < 18:
-                        ficha = Ficha(fil,col,self.turn)
-                        self.table.board[fil][col]=ficha
+                        ficha = Ficha(fil, col, self.turn)
+                        self.table.board[fil][col] = ficha
                         self.cambiar_turno()
-                        self.contador = self.contador +1 #Numero de fichas
-                        self.table.verificar_molino()
+                        self.contador = self.contador + 1  # Numero de fichas
+                        self.table.check_mill()
         return False
-    
