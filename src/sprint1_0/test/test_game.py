0a916a6af2f35c4495d448b4b89462abb3ad183f
JDomCamZ <84546063+JDomCamZ@users.noreply.github.com>
2022-05-11 23:35:05 -0500

    Implementing game test.

---
+++ src/sprint1_0/test/test_game.py
@@ -0,0 +1,20 @@
+import unittest
+import sys
+
+sys.path.append("..")
+
+from product.constants import GREY
+from product.game import *
+
+class TestGame(unittest.TestCase):
+    def test_cambiar_turno(self):
+        screen = pygame.display.set_mode([1000,1000])
+        g=Game(screen)
+        g.cambiar_turno()
+        self.assertEqual(g.turn,BROWN)
+    def test_cambiar_turno2(self):
+        screen = pygame.display.set_mode([1000,1000])
+        g=Game(screen)
+        g.cambiar_turno()
+        g.cambiar_turno()
+        self.assertEqual(g.turn,GREY)
