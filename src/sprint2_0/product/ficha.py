13d0f1d24c2006015c96da6b3fd89e0e6afba193
Hiteek <ruben.ricapa.c@uni.pe>
2022-05-22 01:47:36 -0500

    Variable name changed

--- src/sprint2_0/product/ficha.py
+++ src/sprint2_0/product/ficha.py
@@ -1,29 +1,27 @@
 import pygame
 import sys
 from constants import SIZE
-# CLASE FICHA
 sys.path.append(
     "D:\Programas\Pygame\Proyecto Software\Proyecto-Desarrollo-Software")
 
 
-class Ficha():
+class Piece():
     def __init__(self, fil, col, color):
         self.fil = fil
         self.col = col
         self.color = color
         self.x = 0
         self.y = 0
-        self.calcular_posicion()
-    # Calcula la posicion en la pantalla
+        self.calculate_position()
 
-    def calcular_posicion(self):
+    def calculate_position(self):
         self.x = SIZE*self.col + SIZE//2
         self.y = SIZE*self.fil + SIZE//2
 
     def move(self, fil, col):
         self.fil = fil
         self.col = col
-        self.calcular_posicion()
+        self.calculate_position()
 
     def draw(self, screen):
         pygame.draw.circle(screen, self.color, (self.x, self.y), 20)
